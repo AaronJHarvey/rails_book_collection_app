e3626eb905a738033d3195baf9e0cc8dbd00b027
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-09-15 07:03:22 -0400

    Allows user to see genres they have books in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to books_path
+    @user_genres = @user.genres.uniq
+    render :show
   end
 
   private
